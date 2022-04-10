d94d7657f7700a6fc74c7e93c246bc1c6c1bcaa6
Ivan Gil <grxivan@gmail.com>
2022-04-10 11:07:06 +0200

    Eliminada importación no utilizada

--- src/components/common/PoweredBy.js
+++ src/components/common/PoweredBy.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {ReactComponent as GhostLogo} from '../../images/ghost-logo-small.svg';
 
 export default class PoweredBy extends React.Component {
     render() {
